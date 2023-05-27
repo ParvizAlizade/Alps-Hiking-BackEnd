cf47e581e2e80384c848d31ce70546e6eb3cb70d
ParvizAlizade <perviz.elizade.2002@mail.ru>
2023-05-27 18:45:53 +0400

    Front to back and Login Register System

---
+++ Alps Hiking/DAL/AlpsHikingDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Alps_Hiking.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Alps_Hiking.DAL
+{
+	public class AlpsHikingDbContext : IdentityDbContext<User>
+	{
+		public AlpsHikingDbContext(DbContextOptions<AlpsHikingDbContext> options) : base(options)
+		{
+
+		}
+
+		public DbSet<Setting> Settings { get; set; }
+        public DbSet<Slider> Sliders { get; set; }
+        public DbSet<Destiantion> Destiantions { get; set; }
+        public DbSet<Tour> Tours { get; set; }
+        public DbSet<TourImage> TourImages { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Itinerary> Itineraries { get; set; }
+        public DbSet<PassengerCount>PassengerCounts { get; set; }
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<TourDate> TourDates { get; set; }
+        public DbSet<Partner> Partners { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Setting>()
+				.HasIndex(s => s.Key)
+				.IsUnique();
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+}
